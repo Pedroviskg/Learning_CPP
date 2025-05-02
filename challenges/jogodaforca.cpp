39993ce6c6064af4efdadbf0499c43416bcbc2b8
Pedro Gabriel Barroso <145069336+Pedroviskg@users.noreply.github.com>
2025-05-02 17:43:49 -0300

    Update jogodaforca.cpp
    Adding new Portuguese words and making the game playable again if the user wins or loses.

--- challenges/jogodaforca.cpp
+++ challenges/jogodaforca.cpp
@@ -1,16 +1,30 @@
-//VERSÃO 2
 #include <iostream>
 #include <vector>
-#include "Random.cpp"
+#include <random>
 
 namespace wordlist
 {
-std::vector<std::string> wordlist { "mystery", "broccoli" , "account", "almost", "spaghetti", "opinion", "beautiful", "distance", "luggage" };
+std::vector<std::string> wordlist {
+    "amizade", "amor", "infancia", "amada", "livro", "rosa", "principe",
+    "janela", "amigo", "felicidade", "amiga", "cor",
+    "chuva", "montanha", "viagem", "praia", "caminho",
+    "tempo", "historia", "sabedoria", "brisa", "vento",
+    "paz", "musica", "alegria", "noite", "luz",
+    "sol", "lua", "estrela", "floresta", "neblina",
+    "rosto", "sorriso", "olhar", "palavra", "abraço",
+    "esperança", "sonho", "verdade", "verso", "riso",
+    "coração", "segredo", "tempo", "futuro", "passado",
+    "presente", "encanto", "misterio", "viola", "pintura",
+    "cores", "cheiro", "comida", "memória", "jardim"
+};
 }
 
 std::string getword()
 {
-	int k = Random::get(0, 8);
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> distrib(0, 56);
+	int k = distrib(gen);
 	std::string word = wordlist::wordlist[k];
 	return word;
 }
@@ -25,13 +39,12 @@ bool guesstheword(std::string& word)
 	std::vector<char> charchose(1);
 	std::vector<char> chances = {'_','+','+','+','+','+'};
 	int k = 5;
-	
+
 
 	for (int i = 0; i <= size_of_word; i++)
 	{
 		complete.push_back('_');
 	}
-	std::cout << word << std::endl;
 
 	while (counter <= 5)
 	{
@@ -119,18 +132,28 @@ return ganhou;
 
 int main()
 {
+    int cont = 0;
+    while(cont == 0)
+    {
 	std::cout << "Bem vindo ao Jogo da Forca!" << std::endl;
-	std::cout << "Ganha se você acertar a palavra em até 5 tentativas. Bom jogo!";
+	std::cout << "Ganha se você acertar a palavra em até 5 tentativas. Bom jogo!" << std::endl;
 
 	std::string word = getword();
 	bool ver = guesstheword(word);
 	if (ver == true)
 	{
 		std::cout << std::endl << "Ganhou!" << std::endl;
+        std::cout << "Digite 1 para jogar novamente ou 0 para sair:" << std::endl;
+        std::cin  >> ver;
+        std::cout << std::endl;
 	}
 	else
 	{
 		std::cout << std::endl << "Perdeu!" << std::endl;
+        std::cout << "Digite 1 para jogar novamente ou 0 para sair:" << std::endl;
+        std::cin  >> ver;
+        std::cout << std::endl;
 	}
+    };
 	return 0;
 }
